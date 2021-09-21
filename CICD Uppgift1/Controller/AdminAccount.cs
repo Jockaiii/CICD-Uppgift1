7f95af284fd9907fb6fcb5594e9c4ae3c075f0aa
00joaand <00jooand@gafe.molndal.se>
2021-09-21 12:14:09 +0200

    Added missing methods for AdminAccount.cs

--- CICD Uppgift1/Controller/AdminAccount.cs
+++ CICD Uppgift1/Controller/AdminAccount.cs
@@ -6,6 +6,29 @@ namespace CICD_Uppgift1.Controller
 {
     class AdminAccount : Account
     {
+        public static void CheckAccounts()
+        {
 
+        }
+
+        public static void CheckAccountRequests()
+        {
+
+        }
+
+        public static void AdvanceSalaraySystem()
+        {
+
+        }
+
+        public static void CreateLocalAccount()
+        {
+
+        }
+
+        public static void RemoveAccount()
+        {
+
+        }
     }
 }
