7e2fc6551a65f5147fbc5a801697ec193b58cc74
00joaand <00jooand@gafe.molndal.se>
2021-09-21 12:04:00 +0200

    Added methods for database queries

---
+++ CICD Uppgift1/Controller/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CICD_Uppgift1.Controller
+{
+    class Account
+    {
+        public static void CheckAccountBalance()
+        {
+
+        }
+
+        public static void CheckSalary()
+        {
+
+        }
+
+        public static void CheckRole()
+        {
+
+        }
+
+        public static void RequestSalaryChange()
+        {
+
+        }
+
+        public static void RequestRoleChange()
+        {
+
+        }
+
+        public static  void RemoveAccount()
+        {
+
+        }
+    }
+}
