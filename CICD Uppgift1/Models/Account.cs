07b2bff93eea9d68e7a7f721c34cfefdfc0b3b16
00joaand <00jooand@gafe.molndal.se>
2021-10-01 14:12:19 +0200

    Fixed more incorrect access modifiers

--- CICD Uppgift1/Models/Account.cs
+++ CICD Uppgift1/Models/Account.cs
@@ -6,10 +6,10 @@ namespace CICD_Uppgift1.Models
     {
         [Key]
         public string UserName { get; set; }
-        protected internal virtual string Password { get; set; }
-        protected internal virtual int Balance { get; set; }
-        protected internal virtual int Salary { get; set; }
-        protected internal virtual string Role { get; set; }
+        public virtual string Password { get; set; }
+        public virtual int Balance { get; set; }
+        public virtual int Salary { get; set; }
+        public virtual string Role { get; set; }
 
         protected internal abstract bool GetAccountDetails(string userName);
     }
