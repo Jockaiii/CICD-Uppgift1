f62bbfa25e2b4eba41d0049f4bf24396e8ef11a9
00joaand <00jooand@gafe.molndal.se>
2021-10-04 18:04:31 +0200

    Switch from comparing poll.Role != null to poll.Role != "" to represent the changes made in RequestPoll concstructor

--- CICD Uppgift1/Views/ConsoleView.cs
+++ CICD Uppgift1/Views/ConsoleView.cs
@@ -197,7 +197,7 @@ namespace CICD_Uppgift1.Views
             foreach (var poll in requestPolls)
             {
                 count++;
-                if (poll.Role != null)
+                if (poll.Role != "")
                 {
                     Console.WriteLine($"[{count}] {poll.Username} has requested to change their role from {poll.OldRole} to {poll.Role}\nDo you want to accept this request?\n [1] Yes\n [2] No");
                     switch (Console.ReadLine())
