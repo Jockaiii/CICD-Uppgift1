692b64c829038cfff63748c1d5837d000bb04120
Joakim Andersson <joakimandersson10@gmail.com>
2021-10-11 08:23:43 +0200

    added program tests

--- CICD Uppgift1Tests/ProgramTests.cs
+++ CICD Uppgift1Tests/ProgramTests.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using NUnit.Framework;
+using CICD_Uppgift1;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CICD_Uppgift1Tests
+namespace CICD_Uppgift1.Tests
 {
-    class ProgramTests
+    [TestFixture()]
+    public class ProgramTests
     {
+        [Test()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
     }
 }
